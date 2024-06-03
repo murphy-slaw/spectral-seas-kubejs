b38699cdd78f5584c55a77e79cdf679021f36b06
Kevin Murphy <kevin@eorbit.net>
2024-06-03 11:54:10 -0700

    Tweak Small Ships recipes

--- server_scripts/recipes.js
+++ server_scripts/recipes.js
@@ -1,5 +1,30 @@
 ServerEvents.recipes(event =>{
     event.remove({output: '#minecraft:boats'})
+
+    event.replaceInput(
+        {output: '#smallships:ships'},
+        'minecraft:lead',
+        'minecraft:leather'
+   )
+
+   event.replaceInput(
+        {output:'smallships:sail'},
+        'minecraft:lead',
+        'minecraft:string'
+   )
+
+    for (const wood of ['acacia','oak','spruce','jungle','dark_oak','birch']){
+        event.replaceInput(
+            {output: '#smallships:ships'},
+            'minecraft:'+ wood + '_boat',
+            'minecraft:stripped_' + wood + '_log'
+        )
+        event.replaceInput(
+            {output: '#smallships:briggs'},
+            'minecraft:'+ wood + '_chest_boat',
+            'minecraft:stripped_' + wood + '_log'
+        )
+    }
     let recipes = []
     event.forEachRecipe({}, r => {
         console.log(r.json.toString())
